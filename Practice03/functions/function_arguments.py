f2f2756d9dda13256d1c37e85963b35b8bfeee45
baktiyar24 <baktiyar.2424@gmail.com>
2026-02-05 10:18:04 +0500

    Arguments

---
+++ Practice03/functions/function_arguments.py
@@ -0,0 +1,19 @@
+"""""
+Arguments
+Information can be passed into functions as arguments.
+Arguments are specified after the function name, inside the parentheses. You can add as many arguments as you want, just separate them with a comma.
+The following example has a function with one argument (fname). 
+When the function is called, we pass along a first name, which is used inside the function to print the full name:
+"""""
+def my_function(fname):
+  print(fname + " Refsnes")
+
+my_function("Emil")
+my_function("Tobias")
+my_function("Linus")
+
+
+def my_function(name): # name is a parameter
+  print("Hello", name)
+
+my_function("Emil") # "Emil" is an argument
