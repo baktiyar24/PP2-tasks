b9ef6239f0badf399aafebb3f71c008c22be15a2
baktiyar24 <baktiyar.2424@gmail.com>
2026-02-27 21:32:34 +0500

    JSON added

--- Practice04/json/Exercise1.py
+++ Practice04/json/Exercise1.py
@@ -1,11 +1,18 @@
 import json
+
 with open("sample-data.json", "r") as file:
     data = json.load(file)
 
-
 print("Interface Status")
-print("==============================================\n")
-print("DN                                           Description   Speed   MTU")
-print("------------------------------------------- -------------- -------------")
+print("================================================================================")
+print("DN                                                 Description          Speed   MTU  ")
+print("-------------------------------------------------- -------------------- ------- -----")
 
+for item in data["imdata"]:
+    attrs = item["l1PhysIf"]["attributes"]
+    dn = attrs["dn"]
+    descr = attrs["descr"]
+    speed = attrs["speed"]
+    mtu = attrs["mtu"]
     
+    print(f"{dn:50} {descr:20} {speed:7} {mtu:6}")
